7bc281242526f36c6787f1b9ab3ae7d869bf1fde
JoVe13 <jonathansep01@gmail.com>
2026-02-25 18:46:13 +0000

    Added all files from the 1.0.0 version

---
+++ emoProxy_1.0.0/opt/emoProxy/scripts/install_deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+pkexec bash -c "
+apt update
+apt install -y docker.io docker-compose golang
+systemctl enable docker
+systemctl start docker
+usermod -aG docker $USER
+"
---
+++ emoProxy_1.0.0/opt/emoProxy/scripts/start_proxy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+cd "$HOME/emoProxy/Proxy"
+sudo go run .
